5235527d992f18ff0d1cae04210500f7208850cb
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-02-18 09:13:34 -0500

    work in progress

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
+var deck =[];
+
+
+
+
+
+
+
+
 var game = {
   deck: [],
   players: [],
   hands: [],
   buildDeck: function(){
+    values.foreach(function(value) {suits.forEach(function(suit){
+      game.deck.push (value + " " + suit)
+    });
 
   },
   shuffleDeck: function(){
